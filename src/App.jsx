6108339b90bc17187e6ae2a34a03ed1fbcf23f7d
akash-sah-aks <akashsah.aks@gmail.com>
2024-03-07 17:12:42 +0530

    added :: Header, footer components, setup redux toolkit for login logout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,46 @@
-import conf from "./conf/conf";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from "./components/index";
+import { Outlet } from "react-router-dom";
+
 function App() {
-   console.log(conf.appwriteUrl);
-   return (
+   const [loading, setLoading] = useState(true);
+   const dispatch = useDispatch();
+
+   useEffect(() => {
+      authService
+         .getCurrentUser()
+         .then((userData) => {
+            if (userData) {
+               dispatch(login({ userData }));
+            } else {
+               dispatch(logout());
+            }
+         })
+         .catch((error) => {
+            console.log("error : ", error);
+         })
+         .finally(() => setLoading(false));
+   }, []);
+
+   return !loading ? (
       <>
-         <h1 className="font-bold text-3xl">My Blog</h1>
+         <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+            <div className="w-full block">
+               <Header />
+               <main>
+                  {/* Todo */}
+                  <Outlet />
+               </main>
+               <Footer />
+            </div>
+         </div>
+      </>
+   ) : (
+      <>
+         <h1 className="font-bold text-3xl">Loading</h1>
       </>
    );
 }
