afee8db30611767cf206d94c1fa13404a5cce887
akash-sah-aks <akashsah.aks@gmail.com>
2024-03-19 09:40:08 +0530

    added :: PostCard and Select component

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,23 @@
+// import
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./container/Container.jsx";
 import Logo from "./Logo.jsx";
 import LogoutBtn from "./Header/LogoutBtn.jsx";
 import Button from "./Button.jsx";
-export { Header, Footer, Container, Logo, LogoutBtn, Button };
+import Input from "./Input.jsx";
+import Select from "./Select.jsx";
+import PostCard from "./PostCard.jsx";
+
+// export
+export {
+   Header,
+   Footer,
+   Container,
+   Logo,
+   LogoutBtn,
+   Button,
+   Input,
+   Select,
+   PostCard,
+};
